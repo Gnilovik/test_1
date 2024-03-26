d00695ab5822d03adfac168badcd2184fc2b3c79
Gnilovik <black.sabbath97@mail.ru>
2024-03-26 10:27:37 +0300

    Delete calculator.py

--- calculator.py
+++ calculator.py
@@ -87,15 +87,3 @@ try:
     b = int(b)
     if 1 <= a <= 10 and 1 <= b <=10 and c == '+':
         print(a+b)
-    elif 1 <= a <= 10 and 1 <= b <=10 and c == '-':
-        print(a-b)
-    elif 1 <= a <= 10 and 1 <= b <=10 and c == '*':
-        print(a*b)
-    # elif a==0 or b==0 and c=='/':
-    #     print('На ноль делить нельзя!')
-    elif 1 <= a <= 10 and 1 <= b <=10 and c == '/':
-        print(a//b)
-    else:
-        print('Не верно!')
-except Exception:
-    print('Формат математической операции не верный!')
